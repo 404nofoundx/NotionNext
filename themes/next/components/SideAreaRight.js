cb1a955ba0f49769a790270a9855ca9a3f90be58
zhanxujia <zhanxujia@baidu.com>
2024-07-15 14:47:36 +0800

    更新广告ad-slot

--- themes/next/components/SideAreaRight.js
+++ themes/next/components/SideAreaRight.js
@@ -35,8 +35,8 @@ const SideAreaRight = (props) => {
                 className='adsbygoogle'
                 style={{ display: 'block' }}
                 data-adtest='on'
-                data-ad-client='ca-pub-2708419466378217'
-                data-ad-slot='8807314373'
+                data-ad-client='ca-pub-9761311334785754'
+                data-ad-slot='1559931316'
                 data-ad-format='auto'
                 data-full-width-responsive='true'
             />
